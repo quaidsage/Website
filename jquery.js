50e3a9623333a9c74a45a8d09d7a8cdbbed2407f
Quaid Sage <qsag699@aucklanduni.ac.nz>
2023-09-02 18:05:27 +1200

    feat: Add animation to reset project animation
    Pressing project button again will return the navigation to its
    original position and will hide content previously revealed in future.

--- jquery.js
+++ jquery.js
@@ -1,9 +1,21 @@
-$(window).on("load",function(){
+$(window).on("load", function () {
     $(".loader-wrapper").fadeOut("slow");
     $("body").removeClass("preload");
 });
-$(document).on("ready",function() {
-    $("#btn-projects-ascii").on("click",function() {
-        $('#nav').css('left', '-20%');
+
+$(document).on("ready", function () {
+    var a = 0;
+    $("#btn-projects-ascii").on("click", function () {
+        if (a == 0) {
+            $('#nav').css('left', '-20%');
+            $( "<style>#btn-projects-ascii pre:hover:before {content: '\\00a0_________________\\a/                 \\\\\\a|  <-projects--   |\\a \\\\_________________/';}</style>" ).appendTo( "head" )
+            a = 1;
+        }
+        else {
+            $('#nav').css('left', '0%');
+            $( "<style>#btn-projects-ascii pre:hover:before {content: '\\00a0_________________\\a/                 \\\\\\a|  --projects->   |\\a \\\\_________________/';}</style>" ).appendTo( "head" )
+            a = 0;
+        }
+        
     });
 });
