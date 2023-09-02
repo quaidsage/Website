767ca9ddef2dc25fe78e41450af865710670c849
Quaid Sage <qsag699@aucklanduni.ac.nz>
2023-09-02 17:26:04 +1200

    feat: Add animation to nav via project button
    Removes projects.html and adds animation to nav via when project button
    is clicked. The animation moves the nav to the left to make way for
    future information relevant to projects to be added.

--- jquery.js
+++ jquery.js
@@ -2,3 +2,8 @@ $(window).on("load",function(){
     $(".loader-wrapper").fadeOut("slow");
     $("body").removeClass("preload");
 });
+$(document).on("ready",function() {
+    $("#btn-projects-ascii").on("click",function() {
+        $('#nav').css('left', '-20%');
+    });
+});
