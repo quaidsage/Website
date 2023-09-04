8faca42fe8ff610c8a7e91249611fdfaeeb3a8ed
Quaid Sage <qsag699@aucklanduni.ac.nz>
2023-09-04 14:56:36 +1200

    feat: Add control buttons to project tab
    Adds three (left, right, see all) control buttons which will control
    the content sections howing the projects and relevant details.

--- jquery.js
+++ jquery.js
@@ -7,7 +7,7 @@ $(document).on("ready", function () {
     var a = 0;
     $("#btn-projects-ascii").on("click", function () {
         if (a == 0) {
-            $('#nav-container').css('left', '-30%');
+            $('#nav-container').css('left', '-25%');
             $("<style>#btn-projects-ascii pre:hover:before {content: '\\00a0_________________\\a/                 \\\\\\a|  <-projects--   |\\a \\\\_________________/';}</style>").appendTo("head")
             a = 1;
             $('#projects-container').css('left', '40%');
