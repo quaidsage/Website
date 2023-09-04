dfc4676a6c4c3c87ec4b7d358b2d522cd8fc5542
Quaid Sage <qsag699@aucklanduni.ac.nz>
2023-09-04 13:33:04 +1200

    fix: Fix alignment of nav for all screen sizes
    Fixes the navigation background and navigation buttons not aligning
    correctly with the center of the webpage. This also impacted the
    positioning of the projects tab which is now fixed.

--- jquery.js
+++ jquery.js
@@ -7,13 +7,13 @@ $(document).on("ready", function () {
     var a = 0;
     $("#btn-projects-ascii").on("click", function () {
         if (a == 0) {
-            $('#content').css('left', '10%');
+            $('#nav-container').css('left', '-30%');
             $("<style>#btn-projects-ascii pre:hover:before {content: '\\00a0_________________\\a/                 \\\\\\a|  <-projects--   |\\a \\\\_________________/';}</style>").appendTo("head")
             a = 1;
             $('#projects-container').css('left', '40%');
         }
         else {
-            $('#content').css('left', '0%');
+            $('#nav-container').css('left', '0');
             $("<style>#btn-projects-ascii pre:hover:before {content: '\\00a0_________________\\a/                 \\\\\\a|  --projects->   |\\a \\\\_________________/';}</style>").appendTo("head")
             a = 0;
             $('#projects-container').css('left', '100%');
