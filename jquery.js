18ab828d5a97f46478d7a74e6f5a1bd4977178a4
Quaid Sage <qsag699@aucklanduni.ac.nz>
2024-03-12 22:27:16 +1300

    Update CSS styles and project showcase content

--- jquery.js
+++ jquery.js
@@ -40,30 +40,34 @@ $(document).on("ready", function () {
     /* Opens project showcase 1 */
     $("#projects-list-content-proj1").on("click", function () {
         revealShowcase();
-        $("#project-showcase-content-title pre").text("Lost In Time");
-        $("#project-showcase-content-desc pre").text("The description of Lost In Time");
+        $("#project-showcase-content-title").append("<pre>Lost In Time</pre>");
+        $("#project-showcase-content-desc").append("<pre>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia est vitae mi pharetra commodo. Sed malesuada arcu malesuada purus sodales varius. Aliquam et condimentum mauris, non consequat lacus. Pellentesque libero libero, commodo vel dui in, faucibus fringilla justo. Curabitur porta enim in laoreet consectetur. Mauris molestie feugiat ligula non fermentum. Quisque ac mattis lacus. Maecenas semper pharetra ipsum eget fringilla. Donec et condimentum nibh. Phasellus non erat accumsan, laoreet mauris a, mattis sem. Integer rhoncus ante non nisi euismod dictum. In eget neque faucibus, congue ex dignissim, euismod risus. Nam convallis ex et sodales sagittis. Proin suscipit dui sed arcu sodales, sit amet fringilla ex hendrerit.</pre>");
+        $("#project-showcase-content-desc").append("<pre>Donec ullamcorper justo eu porta dapibus. Nam id risus ex. Phasellus quis odio non risus semper ornare quis non metus. Donec varius venenatis nisi ac consectetur. Morbi eget mauris id nisl tristique congue sed quis risus. Vestibulum facilisis mauris ut dignissim tristique. Vestibulum efficitur massa in mauris aliquet, vitae accumsan sapien venenatis. Suspendisse dignissim ex quis ex pretium, in venenatis ipsum tincidunt. Suspendisse potenti. Aliquam non placerat ex. Morbi efficitur placerat varius. Nunc mattis velit mauris, a condimentum mauris imperdiet sit amet.</pre>");
         $("#project-showcase-content-github-btn-ascii a").attr("href", "https://github.com/quaidsage/Lost-In-Time");
     });
 
     /* Opens project showcase 2 */
     $("#projects-list-content-proj2").on("click", function () {
         revealShowcase();
-        $("#project-showcase-content-title pre").text("OpenGL Project");
-        $("#project-showcase-content-desc pre").text("The description of OpenGL Project");
+        $("#project-showcase-content-title").append("<pre>OpenGL Project</pre>");
+        $("#project-showcase-content-desc").append("<pre>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia est vitae mi pharetra commodo. Sed malesuada arcu malesuada purus sodales varius. Aliquam et condimentum mauris, non consequat lacus. Pellentesque libero libero, commodo vel dui in, faucibus fringilla justo. Curabitur porta enim in laoreet consectetur. Mauris molestie feugiat ligula non fermentum. Quisque ac mattis lacus. Maecenas semper pharetra ipsum eget fringilla. Donec et condimentum nibh. Phasellus non erat accumsan, laoreet mauris a, mattis sem. Integer rhoncus ante non nisi euismod dictum. In eget neque faucibus, congue ex dignissim, euismod risus. Nam convallis ex et sodales sagittis. Proin suscipit dui sed arcu sodales, sit amet fringilla ex hendrerit.</pre>");
+        $("#project-showcase-content-desc").append("<pre>Donec ullamcorper justo eu porta dapibus. Nam id risus ex. Phasellus quis odio non risus semper ornare quis non metus. Donec varius venenatis nisi ac consectetur. Morbi eget mauris id nisl tristique congue sed quis risus. Vestibulum facilisis mauris ut dignissim tristique. Vestibulum efficitur massa in mauris aliquet, vitae accumsan sapien venenatis. Suspendisse dignissim ex quis ex pretium, in venenatis ipsum tincidunt. Suspendisse potenti. Aliquam non placerat ex. Morbi efficitur placerat varius. Nunc mattis velit mauris, a condimentum mauris imperdiet sit amet.</pre>");
     });
 
     /* Opens project showcase 3 */
     $("#projects-list-content-proj3").on("click", function () {
         revealShowcase();
-        $("#project-showcase-content-title pre").text("Plagarism Checker");
-        $("#project-showcase-content-desc pre").text("The description of Plagarism Checker");
+        $("#project-showcase-content-title").append("<pre>Plagarism Checker</pre>");
+        $("#project-showcase-content-desc").append("<pre>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia est vitae mi pharetra commodo. Sed malesuada arcu malesuada purus sodales varius. Aliquam et condimentum mauris, non consequat lacus. Pellentesque libero libero, commodo vel dui in, faucibus fringilla justo. Curabitur porta enim in laoreet consectetur. Mauris molestie feugiat ligula non fermentum. Quisque ac mattis lacus. Maecenas semper pharetra ipsum eget fringilla. Donec et condimentum nibh. Phasellus non erat accumsan, laoreet mauris a, mattis sem. Integer rhoncus ante non nisi euismod dictum. In eget neque faucibus, congue ex dignissim, euismod risus. Nam convallis ex et sodales sagittis. Proin suscipit dui sed arcu sodales, sit amet fringilla ex hendrerit.</pre>");
+        $("#project-showcase-content-desc").append("<pre>Donec ullamcorper justo eu porta dapibus. Nam id risus ex. Phasellus quis odio non risus semper ornare quis non metus. Donec varius venenatis nisi ac consectetur. Morbi eget mauris id nisl tristique congue sed quis risus. Vestibulum facilisis mauris ut dignissim tristique. Vestibulum efficitur massa in mauris aliquet, vitae accumsan sapien venenatis. Suspendisse dignissim ex quis ex pretium, in venenatis ipsum tincidunt. Suspendisse potenti. Aliquam non placerat ex. Morbi efficitur placerat varius. Nunc mattis velit mauris, a condimentum mauris imperdiet sit amet.</pre>");
     });
 
     /* Opens project showcase 4 */
     $("#projects-list-content-proj4").on("click", function () {
         revealShowcase();
-        $("#project-showcase-content-title pre").text("Fullstack Web Application");
-        $("#project-showcase-content-desc pre").text("The description of Fullstack Web Application");
+        $("#project-showcase-content-title").append("<pre>Fullstack Web Application</pre>");
+        $("#project-showcase-content-desc").append("<pre>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia est vitae mi pharetra commodo. Sed malesuada arcu malesuada purus sodales varius. Aliquam et condimentum mauris, non consequat lacus. Pellentesque libero libero, commodo vel dui in, faucibus fringilla justo. Curabitur porta enim in laoreet consectetur. Mauris molestie feugiat ligula non fermentum. Quisque ac mattis lacus. Maecenas semper pharetra ipsum eget fringilla. Donec et condimentum nibh. Phasellus non erat accumsan, laoreet mauris a, mattis sem. Integer rhoncus ante non nisi euismod dictum. In eget neque faucibus, congue ex dignissim, euismod risus. Nam convallis ex et sodales sagittis. Proin suscipit dui sed arcu sodales, sit amet fringilla ex hendrerit.</pre>");
+        $("#project-showcase-content-desc").append("<pre>Donec ullamcorper justo eu porta dapibus. Nam id risus ex. Phasellus quis odio non risus semper ornare quis non metus. Donec varius venenatis nisi ac consectetur. Morbi eget mauris id nisl tristique congue sed quis risus. Vestibulum facilisis mauris ut dignissim tristique. Vestibulum efficitur massa in mauris aliquet, vitae accumsan sapien venenatis. Suspendisse dignissim ex quis ex pretium, in venenatis ipsum tincidunt. Suspendisse potenti. Aliquam non placerat ex. Morbi efficitur placerat varius. Nunc mattis velit mauris, a condimentum mauris imperdiet sit amet.</pre>");
     });
 
     /* Closes project showcase tab*/
@@ -74,6 +78,11 @@ $(document).on("ready", function () {
         $('#projects-list-container').css('top', '25%');
         $("#nav-container").fadeOut("fast");
         $('body').css('overflow-y', 'visible');
+
+        setTimeout(function() {
+            $("#project-showcase-content-title").empty();
+            $("#project-showcase-content-desc").empty();
+        }, 100);
     });
 
     /* Alert about me coming soon */
