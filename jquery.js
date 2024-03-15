b9e37429de90589e07c37487aad0bcec5c47c122
qsag699 <qsag699@aucklanduni.ac.nz>
2024-03-15 14:22:32 +1300

    Fix issues arisen from prior merge

--- jquery.js
+++ jquery.js
@@ -72,9 +72,6 @@ $(document).on("ready", function () {
     /* Opens project showcase 3 */
     $("#projects-list-content-proj3").on("click", function () {
         revealShowcase();
-        $("#project-showcase-content-title pre").text("Plagarism Checker");
-        $("#project-showcase-content-desc").append("<pre>The description of Plagarism Checker.</pre>");
-        $("#project-showcase-content-desc").append("<pre>More to be added...</pre>");
         $("#project-showcase-content-title").append("<pre>Plagarism Checker</pre>");
         $("#project-showcase-content-desc").append("<pre>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia est vitae mi pharetra commodo. Sed malesuada arcu malesuada purus sodales varius. Aliquam et condimentum mauris, non consequat lacus. Pellentesque libero libero, commodo vel dui in, faucibus fringilla justo. Curabitur porta enim in laoreet consectetur. Mauris molestie feugiat ligula non fermentum. Quisque ac mattis lacus. Maecenas semper pharetra ipsum eget fringilla. Donec et condimentum nibh. Phasellus non erat accumsan, laoreet mauris a, mattis sem. Integer rhoncus ante non nisi euismod dictum. In eget neque faucibus, congue ex dignissim, euismod risus. Nam convallis ex et sodales sagittis. Proin suscipit dui sed arcu sodales, sit amet fringilla ex hendrerit.</pre>");
         $("#project-showcase-content-desc").append("<pre>Donec ullamcorper justo eu porta dapibus. Nam id risus ex. Phasellus quis odio non risus semper ornare quis non metus. Donec varius venenatis nisi ac consectetur. Morbi eget mauris id nisl tristique congue sed quis risus. Vestibulum facilisis mauris ut dignissim tristique. Vestibulum efficitur massa in mauris aliquet, vitae accumsan sapien venenatis. Suspendisse dignissim ex quis ex pretium, in venenatis ipsum tincidunt. Suspendisse potenti. Aliquam non placerat ex. Morbi efficitur placerat varius. Nunc mattis velit mauris, a condimentum mauris imperdiet sit amet.</pre>");
@@ -83,20 +80,9 @@ $(document).on("ready", function () {
     /* Opens project showcase 4 */
     $("#projects-list-content-proj4").on("click", function () {
         revealShowcase();
-        $("#project-showcase-content-title pre").text("Fullstack Web Application");
+        $("#project-showcase-content-title").append("<pre>Fullstack Web Application</pre>");
         $("#project-showcase-content-desc").append("<pre>The description of Fullstack Web Application.</pre>");
         $("#project-showcase-content-desc").append("<pre>More to be added...</pre>");
-    });
-
-    /* Closes project showcase tab*/
-    $("#project-showcase-btn-back").on("click", function () {
-        $("#project-showcase-container").fadeOut("fast");
-        $('#project-showcase-container').css('top', '100%');
-        $("#projects-list-container").fadeIn("fast");
-        $('#projects-list-container').css('top', '25%');
-        $("#nav-container").fadeOut("fast");
-        $('body').css('overflow-y', 'visible');
-        $("#project-showcase-content-desc").empty();
     });
 
     /* Alert about me coming soon */
